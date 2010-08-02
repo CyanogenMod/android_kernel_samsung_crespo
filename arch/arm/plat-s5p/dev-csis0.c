1155be7c304e1792896afa22186ad58056e1b26c
Hyunwoong Kim <khw0178.kim@samsung.com>
2010-08-02 19:37:49 +0900

    S5PC110: HERRING: Added platform data file for FIMC and register files
    Change-Id: Ib602abe59fdcae937aa05818462f2919af187fb7
    Signed-off-by: Hyunwoong Kim <khw0178.kim@samsung.com>

--- arch/arm/plat-s5p/dev-csis0.c
+++ arch/arm/plat-s5p/dev-csis0.c
@@ -11,6 +11,9 @@
 #include <linux/kernel.h>
 #include <linux/interrupt.h>
 #include <linux/platform_device.h>
+
+#include <plat/csis.h>
+#include <plat/mipi_csis.h>
 #include <mach/map.h>
 
 static struct resource s5p_mipi_csis0_resource[] = {
@@ -32,3 +35,26 @@ struct platform_device s5p_device_mipi_csis0 = {
 	.num_resources	  = ARRAY_SIZE(s5p_mipi_csis0_resource),
 	.resource	  = s5p_mipi_csis0_resource,
 };
+
+static struct s3c_platform_csis default_csis_data __initdata = {
+	.srclk_name	= "mout_mpll",
+	.clk_name	= "sclk_csis",
+	.clk_rate	= 166000000,
+};
+
+void __init s3c_csis_set_platdata(struct s3c_platform_csis *pd)
+{
+	struct s3c_platform_csis *npd;
+
+	if (!pd)
+		pd = &default_csis_data;
+
+	npd = kmemdup(pd, sizeof(struct s3c_platform_csis), GFP_KERNEL);
+	if (!npd)
+		printk(KERN_ERR "%s: no memory for platform data\n", __func__);
+
+	npd->cfg_gpio = NULL;
+	npd->cfg_phy_global = s5p_csis_phy_enable;
+
+	s5p_device_mipi_csis0.dev.platform_data = npd;
+}
