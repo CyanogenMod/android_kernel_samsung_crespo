031dd818e8a1b809eca17bfb3d8523193528d74d
Choi jonghwan <jhbird.choi@samsung.com>
2010-09-15 06:46:02 -0700

    S5PC11X: CLOCK: Change the clk_get function.
    Change the clk_get function. Add use "dev" pointer to find a clock.
    
    Change-Id: I6d456aefa672f2ca46249c380d4f8c0139c97c98
    Signed-off-by: Choi jonghwan <jhbird.choi@samsung.com>

--- arch/arm/plat-samsung/include/plat/clock.h
+++ arch/arm/plat-samsung/include/plat/clock.h
@@ -46,6 +46,7 @@ struct clk {
 	unsigned long         ctrlbit;
 
 	struct clk_ops		*ops;
+	struct device		*dev;
 	int		    (*enable)(struct clk *, int enable);
 #if defined(CONFIG_PM_DEBUG) && defined(CONFIG_DEBUG_FS)
 	struct dentry		*dent;	/* For visible tree hierarchy */
