2687382f8718ce6d61257705d72ad30a443eaba0
Choi jonghwan <jhbird.choi@samsung.com>
2010-10-06 20:58:59 -0700

    S5PC11X: CLOCK: Don't disable lcd clock in booting time.
    Don't disable lcd clock int booting time.
    
    Change-Id: I05ca22a75423e2f4f802dfdbf6baf3b6efce2526
    Signed-off-by: Choi jonghwan <jhbird.choi@samsung.com>

--- arch/arm/mach-s5pv210/clock.c
+++ arch/arm/mach-s5pv210/clock.c
@@ -378,12 +378,6 @@ static struct clk init_clocks_off[] = {
 		.enable		= s5pv210_clk_ip1_ctrl,
 		.ctrlbit	= (1<<17),
 	}, {
-		.name		= "lcd",
-		.id		= -1,
-		.parent		= &clk_hclk_dsys.clk,
-		.enable		= s5pv210_clk_ip1_ctrl,
-		.ctrlbit	= (1<<0),
-	}, {
 		.name		= "jpeg",
 		.id		= -1,
 		.parent		= &clk_hclk_dsys.clk,
@@ -611,6 +605,12 @@ static struct clk init_clocks[] = {
 		.ctrlbit	= (1 << 5),
 		.enable		= s5pv210_clk_ip0_ctrl,
 		.ops		= &clk_hclk_imem_ops,
+	}, {
+		.name		= "lcd",
+		.id		= -1,
+		.parent		= &clk_hclk_dsys.clk,
+		.enable		= s5pv210_clk_ip1_ctrl,
+		.ctrlbit	= (1<<0),
 	}, {
 		.name		= "uart",
 		.id		= 0,
