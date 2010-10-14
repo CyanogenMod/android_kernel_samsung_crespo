2fc746cb09c282d9a049041dbaf03f281f04e1cd
Choi jonghwan <jhbird.choi@samsung.com>
2010-10-14 10:03:14 -0700

    S5PC11X: ONENAND: Now onenand_resume/suspend is called duplicate, remove onenand_resume/suspend in samsung.c
    Now onenand_resume/suspend is called duplicate, remove onenand_resume/suspend in samsung.c.
    Onenand_resume/suspend is called by mtd. Remove s3c_pm_ops_suspend function.
    
    Change-Id: I4bc9016610767decc4d9a7c6553cf01fdc1c21cc
    Signed-off-by: Choi jonghwan <jhbird.choi@samsung.com>

--- drivers/mtd/onenand/onenand_base.c
+++ drivers/mtd/onenand/onenand_base.c
@@ -992,7 +992,7 @@ static int onenand_get_device(struct mtd_info *mtd, int new_state)
 		schedule();
 		remove_wait_queue(&this->wq, &wait);
 	}
-	if (this->clk)
+	if (this->clk && new_state != FL_PM_SUSPENDED)
 		clk_enable(this->clk);
 	return 0;
 }
@@ -1007,6 +1007,9 @@ static void onenand_release_device(struct mtd_info *mtd)
 {
 	struct onenand_chip *this = mtd->priv;
 
+	if (this->clk && this->state != FL_PM_SUSPENDED)
+		clk_disable(this->clk);
+
 	if (this->state != FL_PM_SUSPENDED && this->disable)
 		this->disable(mtd);
 	/* Release the chip */
@@ -1014,8 +1017,6 @@ static void onenand_release_device(struct mtd_info *mtd)
 	this->state = FL_READY;
 	wake_up(&this->wq);
 	spin_unlock(&this->chip_lock);
-	if (this->clk)
-		clk_disable(this->clk);
 }
 
 /**
