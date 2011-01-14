12504f15c1d51e4249df85fe56d3587250f36a72
Todd Poynor <toddpoynor@google.com>
2011-01-13 16:28:34 -0800

    ARM: herring: board changes for max8998 DVS mode
    Change-Id: Ic72e625c7b36119963fbb6ead9c709fdcfeda9f2
    Signed-off-by: Todd Poynor <toddpoynor@google.com>
    
    5PC11X: CPUFREQ: set dvfs voltage platdata
    
    Set arm & int voltage for each cpufreq
    
    Change-Id: Icac7dffe1ca3be49c623053260d5f4c594af3704
    Signed-off-by: Minsu Kim <minsu78.kim@samsung.com>
    
    Fix max8998 platform data for mainline driver
    
    S5PC11x: REGULATOR: add default index for buck1 and buck2
    
    default bootup freq is 800Mhz and its corresponding voltage is 1.2v of 0b'01 in buck1
    
    Signed-off-by: Kisoo Yu <ksoo.yu@samsung.com>
    Change-Id: I15f110dc65bcc93185782a0ef84bbcce10403280

--- arch/arm/mach-s5pv210/mach-herring.c
+++ arch/arm/mach-s5pv210/mach-herring.c
@@ -64,6 +64,7 @@
 #ifdef CONFIG_S5PV210_POWER_DOMAIN
 #include <mach/power-domain.h>
 #endif
+#include <mach/cpu-freq-v210.h>
 
 #include <media/s5ka3dfx_platform.h>
 #include <media/s5k4ecgx.h>
@@ -382,6 +383,37 @@ static struct s5p_media_device herring_media_devs[] = {
 	},
 };
 
+#ifdef CONFIG_CPU_FREQ
+static struct s5pv210_cpufreq_voltage smdkc110_cpufreq_volt[] = {
+	{
+		.freq	= 1000000,
+		.varm	= 1275000,
+		.vint	= 1100000,
+	}, {
+		.freq	=  800000,
+		.varm	= 1200000,
+		.vint	= 1100000,
+	}, {
+		.freq	=  400000,
+		.varm	= 1050000,
+		.vint	= 1100000,
+	}, {
+		.freq	=  200000,
+		.varm	=  950000,
+		.vint	= 1100000,
+	}, {
+		.freq	=  100000,
+		.varm	=  950000,
+		.vint	= 1000000,
+	},
+};
+
+static struct s5pv210_cpufreq_data smdkc110_cpufreq_plat = {
+	.volt	= smdkc110_cpufreq_volt,
+	.size	= ARRAY_SIZE(smdkc110_cpufreq_volt),
+};
+#endif
+
 static struct regulator_consumer_supply ldo3_consumer[] = {
 	REGULATOR_SUPPLY("pd_io", "s3c-usbgadget")
 };
@@ -641,7 +673,6 @@ static struct regulator_init_data herring_buck1_data = {
 	.consumer_supplies	= buck1_consumer,
 };
 
-
 static struct regulator_init_data herring_buck2_data = {
 	.constraints	= {
 		.name		= "VDD_INT",
@@ -812,6 +843,18 @@ static struct max8998_platform_data max8998_pdata = {
 	.num_regulators = ARRAY_SIZE(herring_regulators),
 	.regulators     = herring_regulators,
 	.charger        = &herring_charger,
+	/* Preloads must be in increasing order of voltage value */
+	.buck1_voltage4	= 950000,
+	.buck1_voltage3	= 1050000,
+	.buck1_voltage2	= 1200000,
+	.buck1_voltage1	= 1275000,
+	.buck2_voltage2	= 1000000,
+	.buck2_voltage1	= 1100000,
+	.buck1_set1	= GPIO_BUCK_1_EN_A,
+	.buck1_set2	= GPIO_BUCK_1_EN_B,
+	.buck2_set3	= GPIO_BUCK_2_EN,
+	.buck1_default_idx = 1,
+	.buck2_default_idx = 0,
 };
 
 struct platform_device sec_device_dpram = {
@@ -4224,6 +4267,11 @@ static struct platform_device *herring_devices[] __initdata = {
 	&s3c_device_timer[2],
 	&s3c_device_timer[3],
 #endif
+
+#ifdef CONFIG_CPU_FREQ
+	&s5pv210_device_cpufreq,
+#endif
+
 	&sec_device_rfkill,
 	&sec_device_btsleep,
 	&ram_console_device,
@@ -4540,6 +4588,10 @@ static void __init herring_machine_init(void)
 #endif
 #ifdef CONFIG_S5PV210_SETUP_SDHCI
 	s3c_sdhci_set_platdata();
+#endif
+
+#ifdef CONFIG_CPU_FREQ
+	s5pv210_cpufreq_set_platdata(&smdkc110_cpufreq_plat);
 #endif
 
 	regulator_has_full_constraints();
