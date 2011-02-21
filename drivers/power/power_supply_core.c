28fc55c323b0044f6fc5899a8c195d79a38f3366
Lars-Peter Clausen <lars@metafoo.de>
2011-02-21 15:34:19 +0100

    power_supply: Initialize changed_work before calling device_add
    Calling device_add causes an inital uevent for that device to be generated.
    The power_supply uevent function calls the drivers get_property function,
    which might causes the driver to update its state, which again might
    causes the driver to call power_supply_changed(). Since the power_supplys
    changed_work has not been initialized at this point the behavior is
    undefined and can result in an OOPS.
    
    This patch fixes the issue by initializing the power_supplys changed_work
    prior to adding the power_supplys device to the device tree.
    
    Reported-by: Grazvydas Ignotas <notasas@gmail.com>
    Signed-off-by: Lars-Peter Clausen <lars@metafoo.de>
    Tested-by: Grazvydas Ignotas <notasas@gmail.com>

--- drivers/power/power_supply_core.c
+++ drivers/power/power_supply_core.c
@@ -188,6 +188,8 @@ int power_supply_register(struct device *parent, struct power_supply *psy)
 	dev_set_drvdata(dev, psy);
 	psy->dev = dev;
 
+	INIT_WORK(&psy->changed_work, power_supply_changed_work);
+
 	rc = kobject_set_name(&dev->kobj, "%s", psy->name);
 	if (rc)
 		goto kobject_set_name_failed;
@@ -196,7 +198,6 @@ int power_supply_register(struct device *parent, struct power_supply *psy)
 	if (rc)
 		goto device_add_failed;
 
-	INIT_WORK(&psy->changed_work, power_supply_changed_work);
 	spin_lock_init(&psy->changed_lock);
 	wake_lock_init(&psy->work_wake_lock, WAKE_LOCK_SUSPEND, "power-supply");
 
