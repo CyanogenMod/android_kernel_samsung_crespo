aae6346739e0bb71106d149ef2c4aea4656d37f9
Rom Lemarchand <rlemarchand@sta.samsung.com>
2011-03-01 11:41:13 -0800

    ARM: herring: Add macro to check if a WiMax herring is rev 0
    Adding macro to determine whether a herring is a rev 0 version of the
    CDMA/WiMax hardware.
    
    Change-Id: If2a4b9b7868edcbbc3aa20e4d4b8d62ad97f2a61
    Signed-off-by: Rom Lemarchand <rlemarchand@sta.samsung.com>

--- arch/arm/mach-s5pv210/herring.h
+++ arch/arm/mach-s5pv210/herring.h
@@ -13,6 +13,7 @@ void herring_bt_uart_wake_peer(struct uart_port *port);
 extern void s3c_setup_uart_cfg_gpio(unsigned char port);
 
 #define is_cdma_wimax_dev() (!!((system_rev & 0xFFF0) == 0x20))
+#define is_cdma_wimax_rev0() (!!((system_rev & 0xFFFF) == 0x20))
 
 extern struct s5p_panel_data herring_panel_data;
 extern struct s5p_tft_panel_data herring_sony_panel_data;
