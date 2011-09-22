5dc50558fd7fd600765d60bdf809001bb5ba1e99
Russell King <rmk+kernel@arm.linux.org.uk>
2011-09-22 10:32:25 +0100

    ARM: dma-mapping: free allocated page if unable to map
    If the attempt to map a page for DMA fails (eg, because we're out of
    mapping space) then we must not hold on to the page we allocated for
    DMA - doing so will result in a memory leak.
    
    Cc: <stable@kernel.org>
    Reported-by: Bryan Phillippe <bp@darkforest.org>
    Tested-by: Bryan Phillippe <bp@darkforest.org>
    Signed-off-by: Russell King <rmk+kernel@arm.linux.org.uk>

--- arch/arm/mm/dma-mapping.c
+++ arch/arm/mm/dma-mapping.c
@@ -297,6 +297,8 @@ __dma_alloc(struct device *dev, size_t size, dma_addr_t *handle, gfp_t gfp,
 
 	if (addr)
 		*handle = page_to_dma(dev, page);
+	else
+		__dma_free_buffer(page, size);
 
 	return addr;
 }
