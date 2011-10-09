57c36f33beef3a4a2b66db669c8b917c4c8a60ab
KalimochoAz <calimochoazucarado@gmail.com>
2011-10-09 11:20:50 +0200

    Cosmetic changes to cpufreq

--- drivers/cpufreq/cpufreq.c
+++ drivers/cpufreq/cpufreq.c
@@ -33,7 +33,8 @@
 						"cpufreq-core", msg)
 
 /* UV */
-int exp_UV_mV[9] = { 1440000, 1390000, 1330000, 1290000, 1250000, 1200000, 1050000, 950000, 950000};
+int exp_UV_mV[9] = { 1440000, 1390000, 1330000, 1290000, 1250000, 1200000,
+                     1050000, 950000, 950000};
 
 /**
  * The "cpufreq driver" - the arch- or hardware-dependent low
@@ -223,8 +224,8 @@ static void cpufreq_debug_disable_ratelimit(void)
 	spin_unlock_irqrestore(&disable_ratelimit_lock, flags);
 }
 
-void cpufreq_debug_printk(unsigned int type, const char *prefix,return sprintf(buf, "1440mhz: %d mV\\n1200mhz:
-			const char *fmt, ...)
+void cpufreq_debug_printk(unsigned int type, const char *prefix,
+        const char *fmt, ...)
 {
 	char s[256];
 	va_list args;
@@ -646,7 +647,11 @@ static ssize_t show_scaling_setspeed(struct cpufreq_policy *policy, char *buf)
 /* sysfs interface for UV control */
 static ssize_t show_UV_mV_table(struct cpufreq_policy *policy, char *buf) {
 
-	return sprintf(buf, "1440mhz: %d mV\n1300mhz: %d mV\n1200mhz: %d mV\n1100mhz: %d mV\n1000mhz: %d mV\n800mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n", exp_UV_mV[0]/1000, exp_UV_mV[1]/1000, exp_UV_mV[2]/1000, exp_UV_mV[3]/1000, exp_UV_mV[4]/1000, exp_UV_mV[5]/1000, exp_UV_mV[6]/1000, exp_UV_mV[7]/1000, exp_UV_mV[8]/1000);
+	return sprintf(buf, "1440mhz: %d mV\n1300mhz: %d mV\n1200mhz: %d mV\n1100mhz: %d mV\n1000mhz: %d mV\n800mhz: %d mV\n400mhz: %d mV\n200mhz: %d mV\n100mhz: %d mV\n",
+                        exp_UV_mV[0]/1000, exp_UV_mV[1]/1000, exp_UV_mV[2]/1000,
+                        exp_UV_mV[3]/1000, exp_UV_mV[4]/1000, exp_UV_mV[5]/1000,
+                        exp_UV_mV[6]/1000, exp_UV_mV[7]/1000,
+                        exp_UV_mV[8]/1000);
 
 }
 
@@ -655,7 +660,10 @@ static ssize_t store_UV_mV_table(struct cpufreq_policy *policy,
 
 	unsigned int ret = -EINVAL;
 	int i = 0;
-	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d", &exp_UV_mV[0], &exp_UV_mV[1], &exp_UV_mV[2], &exp_UV_mV[3], &exp_UV_mV[4], &exp_UV_mV[5], &exp_UV_mV[6], &exp_UV_mV[7], &exp_UV_mV[8]);
+	ret = sscanf(buf, "%d %d %d %d %d %d %d %d %d",
+                &exp_UV_mV[0], &exp_UV_mV[1], &exp_UV_mV[2],
+                &exp_UV_mV[3], &exp_UV_mV[4], &exp_UV_mV[5],
+                &exp_UV_mV[6], &exp_UV_mV[7], &exp_UV_mV[8]);
 	if(ret != 9) {
 		return -EINVAL;
 	}
