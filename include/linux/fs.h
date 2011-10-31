91e083e73d014e02a2dbaaa4f4373405602927a3
Dan Magenheimer <dan.magenheimer@oracle.com>
2011-10-31 11:02:57 +0100

    fs: add field to superblock to support cleancache
    This second patch of eight in this cleancache series adds a field to
    the generic superblock to squirrel away a pool identifier that is
    dynamically provided by cleancache-enabled filesystems at mount time
    to uniquely identify files and pages belonging to this mounted filesystem.
    
    Details and a FAQ can be found in Documentation/vm/cleancache.txt
    
    [v8: trivial merge conflict update]
    Signed-off-by: Dan Magenheimer <dan.magenheimer@oracle.com>
    Reviewed-by: Jeremy Fitzhardinge <jeremy@goop.org>
    Reviewed-by: Konrad Rzeszutek Wilk <konrad.wilk@oracle.com>
    Cc: Andrew Morton <akpm@linux-foundation.org>
    Cc: Al Viro <viro@ZenIV.linux.org.uk>
    Cc: Matthew Wilcox <matthew@wil.cx>
    Cc: Nick Piggin <npiggin@kernel.dk>
    Cc: Mel Gorman <mel@csn.ul.ie>
    Cc: Rik Van Riel <riel@redhat.com>
    Cc: Jan Beulich <JBeulich@novell.com>
    Cc: Chris Mason <chris.mason@oracle.com>
    Cc: Andreas Dilger <adilger@sun.com>
    Cc: Ted Ts'o <tytso@mit.edu>
    Cc: Mark Fasheh <mfasheh@suse.com>
    Cc: Joel Becker <joel.becker@oracle.com>
    Cc: Nitin Gupta <ngupta@vflare.org>
    
    Conflicts:
    
    	include/linux/fs.h

--- include/linux/fs.h
+++ include/linux/fs.h
@@ -1385,6 +1385,11 @@ struct super_block {
 	 * generic_show_options()
 	 */
 	char *s_options;
+
+	/*
+	 * Saved pool identifier for cleancache (-1 means none)
+	 */
+	int cleancache_poolid;
 };
 
 extern struct timespec current_fs_time(struct super_block *sb);
