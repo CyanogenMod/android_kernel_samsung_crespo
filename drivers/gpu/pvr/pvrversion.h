bc2c825eebf89b176cf15229efd35036b7c469e0
Alistair Strachan <alistair.strachan@imgtec.com>
2011-11-17 17:35:18 +0000

    gpu: pvr: Update to DDK 1.8@779532
    This is a version number change only, which is required for
    compatibility with the user-mode driver.
    
    Change-Id: I8529046b7f78cd99fdb458afca37d96d84b686c8

--- drivers/gpu/pvr/pvrversion.h
+++ drivers/gpu/pvr/pvrversion.h
@@ -36,16 +36,16 @@
 
 #define PVRVERSION_FAMILY           "sgxddk"
 #define PVRVERSION_BRANCHNAME       "1.8"
-#define PVRVERSION_BUILD             779202
-#define PVRVERSION_BSCONTROL        "blaze_android_sgx_ogles1_ogles2_GPL"
+#define PVRVERSION_BUILD             779532
+#define PVRVERSION_BSCONTROL        "CustomerGoogle_Android_ogles1_ogles2_GPL"
 
-#define PVRVERSION_STRING           "blaze_android_sgx_ogles1_ogles2_GPL sgxddk 18 1.8@" PVR_STR2(PVRVERSION_BUILD)
+#define PVRVERSION_STRING           "CustomerGoogle_Android_ogles1_ogles2_GPL sgxddk 18 1.8@" PVR_STR2(PVRVERSION_BUILD)
 #define PVRVERSION_STRING_SHORT     "1.8@" PVR_STR2(PVRVERSION_BUILD)
 
 #define COPYRIGHT_TXT               "Copyright (c) Imagination Technologies Ltd. All Rights Reserved."
 
 #define PVRVERSION_BUILD_HI          77
-#define PVRVERSION_BUILD_LO          9202
+#define PVRVERSION_BUILD_LO          9532
 #define PVRVERSION_STRING_NUMERIC    PVR_STR2(PVRVERSION_MAJ) "." PVR_STR2(PVRVERSION_MIN) "." PVR_STR2(PVRVERSION_BUILD_HI) "." PVR_STR2(PVRVERSION_BUILD_LO)
 
 #endif /* _PVRVERSION_H_ */
