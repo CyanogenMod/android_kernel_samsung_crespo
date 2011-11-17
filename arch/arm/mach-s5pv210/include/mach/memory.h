coolya/android_kernel_samsung

HERRING: INIT: device initialze for kernel booting.
HERRING: Makefile: Change zreload address configuration.

HERRING: Makefile: Bug fix at zreload base.

arch/arm/mach-s5pv210/include/mach/memory.h
@@ -13,11 +13,19 @@
 #ifndef __ASM_ARCH_MEMORY_H
 #define __ASM_ARCH_MEMORY_H
 
+#if defined(CONFIG_MACH_SMDKV210)
 #define PHYS_OFFSET		UL(0x20000000)
+#else
+#define PHYS_OFFSET		UL(0x30000000)
+#endif
+
 #define CONSISTENT_DMA_SIZE	(SZ_8M + SZ_4M + SZ_2M)
 
 /* Maximum of 256MiB in one bank */
 #define MAX_PHYSMEM_BITS	32
 #define SECTION_SIZE_BITS	28
 
+#if defined(CONFIG_MACH_S5PC110_CRESPO)
+#define NODE_MEM_SIZE_BITS   28
+ #endif
 #endif /* __ASM_ARCH_MEMORY_H */

arch/arm/mach-s5pv210/include/mach/vmalloc.h
@@ -17,6 +17,6 @@
 #ifndef __ASM_ARCH_VMALLOC_H
 #define __ASM_ARCH_VMALLOC_H __FILE__
 
-#define VMALLOC_END	  (0xE0000000)
+#define VMALLOC_END	  (0xFC000000)
 
 #endif /* __ASM_ARCH_VMALLOC_H */

arch/arm/mach-s5pv210/mach-herring.c
@@ -1,0 +1,123 @@
+/* linux/arch/arm/mach-s5pv210/mach-herring.c
+ *
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/init.h>
+#include <linux/serial_core.h>
+
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/setup.h>
+#include <asm/mach-types.h>
+
+#include <mach/map.h>
+#include <mach/regs-clock.h>
+
+#include <plat/regs-serial.h>
+#include <plat/s5pv210.h>
+#include <plat/devs.h>
+#include <plat/cpu.h>
+
+/* Following are default values for UCON, ULCON and UFCON UART registers */
+#define S5PV210_UCON_DEFAULT	(S3C2410_UCON_TXILEVEL |	\
+				 S3C2410_UCON_RXILEVEL |	\
+				 S3C2410_UCON_TXIRQMODE |	\
+				 S3C2410_UCON_RXIRQMODE |	\
+				 S3C2410_UCON_RXFIFO_TOI |	\
+				 S3C2443_UCON_RXERR_IRQEN)
+
+#define S5PV210_ULCON_DEFAULT	S3C2410_LCON_CS8
+
+#define S5PV210_UFCON_DEFAULT	(S3C2410_UFCON_FIFOMODE |	\
+				 S5PV210_UFCON_TXTRIG4 |	\
+				 S5PV210_UFCON_RXTRIG4)
+
+static struct s3c2410_uartcfg herring_uartcfgs[] __initdata = {
+	[0] = {
+		.hwport		= 0,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[1] = {
+		.hwport		= 1,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[2] = {
+		.hwport		= 2,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[3] = {
+		.hwport		= 3,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+};
+
+static struct platform_device *herring_devices[] __initdata = {
+	&s5pv210_device_iis0,
+	&s5pv210_device_ac97,
+	&s3c_device_wdt,
+};
+
+static void __init herring_map_io(void)
+{
+	s5p_init_io(NULL, 0, S5P_VA_CHIPID);
+	s3c24xx_init_clocks(24000000);
+	s3c24xx_init_uarts(herring_uartcfgs, ARRAY_SIZE(herring_uartcfgs));
+}
+
+static void __init herring_machine_init(void)
+{
+	platform_add_devices(herring_devices, ARRAY_SIZE(herring_devices));
+}
+
+static void __init herring_fixup(struct machine_desc *desc,
+                                       struct tag *tags, char **cmdline,
+                                       struct meminfo *mi)
+{
+
+	mi->bank[0].start = 0x30000000;
+	mi->bank[0].size = 80 * SZ_1M;
+	mi->bank[0].node = 0;
+
+	mi->bank[1].start = 0x40000000;
+        mi->bank[1].size = 256 * SZ_1M;
+        mi->bank[1].node = 1;
+
+        mi->bank[2].start = 0x50000000;
+        mi->bank[2].size = 128 * SZ_1M;
+        mi->bank[2].node = 2;
+        mi->nr_banks = 3;
+
+}
+
+
+MACHINE_START(SMDKC110, "SMDKC110")
+	/* Maintainer: Kukjin Kim <kgene.kim@samsung.com> */
+	.phys_io	= S3C_PA_UART & 0xfff00000,
+	.io_pg_offst	= (((u32)S3C_VA_UART) >> 18) & 0xfffc,
+	.boot_params	= S5P_PA_SDRAM + 0x100,
+	.fixup		= herring_fixup,
+	.init_irq	= s5pv210_init_irq,
+	.map_io		= herring_map_io,
+	.init_machine	= herring_machine_init,
+	.timer		= &s3c24xx_timer,
+MACHINE_END

arch/arm/plat-samsung/include/plat/map-base.h
@@ -22,7 +22,7 @@
  * an single MOVS instruction (ie, only 8 bits of set data)
  */
 
-#define S3C_ADDR_BASE	(0xF4000000)
+#define S3C_ADDR_BASE	(0xFD000000)
 
 #ifndef __ASSEMBLY__
 #define S3C_ADDR(x)	((void __iomem __force *)S3C_ADDR_BASE + (x))

CyanogenMod/android_kernel_samsung_crespo

Change-Id: I82bb62e0b77b14e0f962377ac246d537bc243c0b

arch/arm/mach-s5pv210/include/mach/memory.h
@@ -13,7 +13,12 @@
 #ifndef __ASM_ARCH_MEMORY_H
 #define __ASM_ARCH_MEMORY_H
 
+#if defined(CONFIG_MACH_SMDKV210)
 #define PLAT_PHYS_OFFSET		UL(0x20000000)
+#else
+#define PLAT_PHYS_OFFSET		UL(0x30000000)
+#endif
+
 #define CONSISTENT_DMA_SIZE	(SZ_8M + SZ_4M + SZ_2M)
 
 /*
@@ -25,4 +30,7 @@
 #define MAX_PHYSMEM_BITS	31
 #define SECTION_SIZE_BITS	28
 
+#if defined(CONFIG_MACH_S5PC110_CRESPO)
+#define NODE_MEM_SIZE_BITS   28
+ #endif
 #endif /* __ASM_ARCH_MEMORY_H */

arch/arm/mach-s5pv210/include/mach/vmalloc.h
@@ -17,6 +17,6 @@
 #ifndef __ASM_ARCH_VMALLOC_H
 #define __ASM_ARCH_VMALLOC_H __FILE__
 
-#define VMALLOC_END	0xF6000000UL
+#define VMALLOC_END	  (0xF6000000)
 
 #endif /* __ASM_ARCH_VMALLOC_H */

arch/arm/mach-s5pv210/mach-herring.c
@@ -1,0 +1,115 @@
+/* linux/arch/arm/mach-s5pv210/mach-herring.c
+ *
+ * Copyright (c) 2010 Samsung Electronics Co., Ltd.
+ *		http://www.samsung.com/
+ *
+ * This program is free software; you can redistribute it and/or modify
+ * it under the terms of the GNU General Public License version 2 as
+ * published by the Free Software Foundation.
+*/
+
+#include <linux/kernel.h>
+#include <linux/types.h>
+#include <linux/init.h>
+#include <linux/serial_core.h>
+
+#include <asm/mach/arch.h>
+#include <asm/mach/map.h>
+#include <asm/setup.h>
+#include <asm/mach-types.h>
+
+#include <mach/map.h>
+#include <mach/regs-clock.h>
+
+#include <plat/regs-serial.h>
+#include <plat/s5pv210.h>
+#include <plat/devs.h>
+#include <plat/cpu.h>
+
+/* Following are default values for UCON, ULCON and UFCON UART registers */
+#define S5PV210_UCON_DEFAULT	(S3C2410_UCON_TXILEVEL |	\
+				 S3C2410_UCON_RXILEVEL |	\
+				 S3C2410_UCON_TXIRQMODE |	\
+				 S3C2410_UCON_RXIRQMODE |	\
+				 S3C2410_UCON_RXFIFO_TOI |	\
+				 S3C2443_UCON_RXERR_IRQEN)
+
+#define S5PV210_ULCON_DEFAULT	S3C2410_LCON_CS8
+
+#define S5PV210_UFCON_DEFAULT	(S3C2410_UFCON_FIFOMODE |	\
+				 S5PV210_UFCON_TXTRIG4 |	\
+				 S5PV210_UFCON_RXTRIG4)
+
+static struct s3c2410_uartcfg herring_uartcfgs[] __initdata = {
+	[0] = {
+		.hwport		= 0,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[1] = {
+		.hwport		= 1,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[2] = {
+		.hwport		= 2,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+	[3] = {
+		.hwport		= 3,
+		.flags		= 0,
+		.ucon		= S5PV210_UCON_DEFAULT,
+		.ulcon		= S5PV210_ULCON_DEFAULT,
+		.ufcon		= S5PV210_UFCON_DEFAULT,
+	},
+};
+
+static struct platform_device *herring_devices[] __initdata = {
+	&s5pv210_device_iis0,
+	&s5pv210_device_ac97,
+	&s3c_device_wdt,
+};
+
+static void __init herring_map_io(void)
+{
+	s5p_init_io(NULL, 0, S5P_VA_CHIPID);
+	s3c24xx_init_clocks(24000000);
+	s3c24xx_init_uarts(herring_uartcfgs, ARRAY_SIZE(herring_uartcfgs));
+}
+
+static void __init herring_machine_init(void)
+{
+	platform_add_devices(herring_devices, ARRAY_SIZE(herring_devices));
+}
+
+static void __init herring_fixup(struct machine_desc *desc,
+                                       struct tag *tags, char **cmdline,
+                                       struct meminfo *mi)
+{
+	mi->bank[0].start = 0x30000000;
+	mi->bank[0].size = 80 * SZ_1M;
+
+	mi->bank[1].start = 0x40000000;
+	mi->bank[1].size = 256 * SZ_1M;
+
+	mi->bank[2].start = 0x50000000;
+	mi->bank[2].size = 128 * SZ_1M;
+	mi->nr_banks = 3;
+}
+
+
+MACHINE_START(HERRING, "herring")
+	.boot_params	= S5P_PA_SDRAM + 0x100,
+	.fixup		= herring_fixup,
+	.init_irq	= s5pv210_init_irq,
+	.map_io		= herring_map_io,
+	.init_machine	= herring_machine_init,
+	.timer		= &s5p_systimer,
+MACHINE_END

arch/arm/plat-samsung/include/plat/map-base.h
@@ -22,7 +22,7 @@
  * an single MOVS instruction (ie, only 8 bits of set data)
  */
 
-#define S3C_ADDR_BASE	0xF6000000
+#define S3C_ADDR_BASE	(0xF6000000)
 
 #ifndef __ASSEMBLY__
 #define S3C_ADDR(x)	((void __iomem __force *)S3C_ADDR_BASE + (x))