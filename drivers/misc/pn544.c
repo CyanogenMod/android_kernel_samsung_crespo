coolya/android_kernel_samsung

pn544: Perform reset to correctly trigger firmware download mode.
The pn544 data sheet is not clear that a full reset with GPIO4 held high is
required to trigger firmware download mode. This fixes as per actual behavior.

Also, increase max buffer size to 512 in order to support firmware download.

Also, increase GPIO delays from 3/7ms (as per data sheet) to 10ms. NXP
engineers insist that 10ms is much more safe.

Change-Id: Ib3f2ad373d6e3768ac26b5532c925d6913fb5ea7
Signed-off-by: Nick Pelly <npelly@google.com>

drivers/misc/pn544.c
@@ -36,7 +36,7 @@
 #include <linux/spinlock.h>
 #include <linux/pn544.h>
 
-#define MAX_BUFFER_SIZE	256
+#define MAX_BUFFER_SIZE	512
 
 struct pn544_dev	{
 	wait_queue_head_t	read_wq;
@@ -178,21 +178,32 @@ static int pn544_dev_ioctl(struct inode *inode, struct file *filp,
 
 	switch (cmd) {
 	case PN544_SET_PWR:
-		if (arg > 1) {  /* enable with firmware download */
+		if (arg == 2) {
+			/* power on with firmware download (requires hw reset)
+			 */
 			pr_info("%s power on with firmware\n", __func__);
-			gpio_set_value(pn544_dev->firm_gpio, 1);
 			gpio_set_value(pn544_dev->ven_gpio, 1);
-			msleep(3);
-		} else if (arg == 1) {  /* enable */
+			gpio_set_value(pn544_dev->firm_gpio, 1);
+			msleep(10);
+			gpio_set_value(pn544_dev->ven_gpio, 0);
+			msleep(10);
+			gpio_set_value(pn544_dev->ven_gpio, 1);
+			msleep(10);
+		} else if (arg == 1) {
+			/* power on */
 			pr_info("%s power on\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 1);
-			msleep(3);
-		} else {
+			msleep(10);
+		} else  if (arg == 0) {
+			/* power off */
 			pr_info("%s power off\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 0);
-			msleep(7);
+			msleep(10);
+		} else {
+			pr_err("%s bad arg %u\n", __func__, arg);
+			return -EINVAL;
 		}
 		break;
 	default:

CyanogenMod/android_kernel_samsung_crespo

drivers/misc/pn544.c
@@ -36,7 +36,7 @@
 #include <linux/spinlock.h>
 #include <linux/pn544.h>
 
-#define MAX_BUFFER_SIZE	256
+#define MAX_BUFFER_SIZE	512
 
 struct pn544_dev	{
 	wait_queue_head_t	read_wq;
@@ -178,21 +178,32 @@ static long pn544_dev_ioctl(struct file *filp,
 
 	switch (cmd) {
 	case PN544_SET_PWR:
-		if (arg > 1) {  /* enable with firmware download */
+		if (arg == 2) {
+			/* power on with firmware download (requires hw reset)
+			 */
 			pr_info("%s power on with firmware\n", __func__);
-			gpio_set_value(pn544_dev->firm_gpio, 1);
 			gpio_set_value(pn544_dev->ven_gpio, 1);
-			msleep(3);
-		} else if (arg == 1) {  /* enable */
+			gpio_set_value(pn544_dev->firm_gpio, 1);
+			msleep(10);
+			gpio_set_value(pn544_dev->ven_gpio, 0);
+			msleep(10);
+			gpio_set_value(pn544_dev->ven_gpio, 1);
+			msleep(10);
+		} else if (arg == 1) {
+			/* power on */
 			pr_info("%s power on\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 1);
-			msleep(3);
-		} else {
+			msleep(10);
+		} else  if (arg == 0) {
+			/* power off */
 			pr_info("%s power off\n", __func__);
 			gpio_set_value(pn544_dev->firm_gpio, 0);
 			gpio_set_value(pn544_dev->ven_gpio, 0);
-			msleep(7);
+			msleep(10);
+		} else {
+			pr_err("%s bad arg %lu\n", __func__, arg);
+			return -EINVAL;
 		}
 		break;
 	default: