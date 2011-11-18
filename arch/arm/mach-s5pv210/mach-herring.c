coolya/android_kernel_samsung

S5PC11X: SENSOR: Add magnetic sensor I2c setting in mach file
Change-Id: Ia9ad8ce4c1a20042708f711a20bd3516bcb054f7
Signed-off-by: Seulki Lee <tim.sk.lee@samsung.com>

arch/arm/mach-s5pv210/mach-herring.c
@@ -1336,6 +1336,12 @@ static struct i2c_board_info i2c_devs9[] __initdata = {
 	},
 };
 
+static struct i2c_board_info i2c_devs12[] __initdata = {
+	{
+		I2C_BOARD_INFO("ak8973b", 0x1c),
+	},
+};
+
 #ifdef CONFIG_DM9000
 static void __init smdkv210_dm9000_set(void)
 {
@@ -2494,6 +2500,7 @@ static struct platform_device *herring_devices[] __initdata = {
 	&s3c_device_i2c6,
 	&s3c_device_i2c7,
 	&s3c_device_i2c9,  /* max1704x:fuel_guage */
+	&s3c_device_i2c12, /* magnetic sensor */
 #ifdef CONFIG_USB_GADGET
 	&s3c_device_usbgadget,
 #endif
@@ -2776,6 +2783,8 @@ static void __init herring_machine_init(void)
 	/* FSA9480 */
 	i2c_register_board_info(7, i2c_devs7, ARRAY_SIZE(i2c_devs7));
 	i2c_register_board_info(9, i2c_devs9, ARRAY_SIZE(i2c_devs9));
+	/* magnetic sensor */
+	i2c_register_board_info(12, i2c_devs12, ARRAY_SIZE(i2c_devs12));
 
 #ifdef CONFIG_FB_S3C_LTE480WV
 	s3cfb_set_platdata(&lte480wv_fb_data);

CyanogenMod/android_kernel_samsung_crespo

arch/arm/mach-s5pv210/mach-herring.c
@@ -1336,6 +1336,12 @@ static struct i2c_board_info i2c_devs9[] __initdata = {
 	},
 };
 
+static struct i2c_board_info i2c_devs12[] __initdata = {
+	{
+		I2C_BOARD_INFO("ak8973b", 0x1c),
+	},
+};
+
 #ifdef CONFIG_DM9000
 static void __init smdkv210_dm9000_set(void)
 {
@@ -2493,6 +2499,7 @@ static struct platform_device *herring_devices[] __initdata = {
 	&herring_i2c6_device,
 	&herring_i2c7_device,
 	&herring_i2c9_device,  /* max1704x:fuel_guage */
+	&herring_i2c12_device, /* magnetic sensor */
 #ifdef CONFIG_USB_GADGET
 	&s3c_device_usbgadget,
 #endif
@@ -2774,6 +2781,8 @@ static void __init herring_machine_init(void)
 	/* FSA9480 */
 	i2c_register_board_info(7, i2c_devs7, ARRAY_SIZE(i2c_devs7));
 	i2c_register_board_info(9, i2c_devs9, ARRAY_SIZE(i2c_devs9));
+	/* magnetic sensor */
+	i2c_register_board_info(12, i2c_devs12, ARRAY_SIZE(i2c_devs12));
 
 #ifdef CONFIG_FB_S3C_LTE480WV
 	s3cfb_set_platdata(&lte480wv_fb_data);