2ea38b47aab0c6a26e981486f7046cf589c04125
Will Tisdale <willtisdale@gmail.com>
2011-12-29 15:33:35 +0000

    samsung: regs-serial: Fix: warning: 'struct uart_port' declared inside parameter list arch/arm/plat-samsung/include/plat/regs-serial.h:284:27: warning: 'struct uart_port' declared inside parameter list [enabled by default] arch/arm/plat-samsung/include/plat/regs-serial.h:284:27: warning: its scope is only this definition or declaration, which is probably not what you want [enabled by default]

--- arch/arm/plat-samsung/include/plat/regs-serial.h
+++ arch/arm/plat-samsung/include/plat/regs-serial.h
@@ -262,6 +262,8 @@ struct s3c24xx_uart_clksrc {
  * arch/arm/mach-s3c2410/ directory.
 */
 
+struct uart_port;
+
 struct s3c2410_uartcfg {
 	unsigned char	   hwport;	 /* hardware port number */
 	unsigned char	   unused;
