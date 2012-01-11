04e99107b0db07334c670cc4b4fbd43cd007cd9a
KalimochoAz <calimochoazucarado@gmail.com>
2012-01-11 15:14:04 +0100

    Dissable fimc clock on resume
    Only if is not fimc 0

--- drivers/media/video/samsung/fimc/fimc_dev.c
+++ drivers/media/video/samsung/fimc/fimc_dev.c
@@ -1619,12 +1619,14 @@ int fimc_resume(struct platform_device *pdev)
 {
 	struct fimc_control *ctrl;
 	struct s3c_platform_fimc *pdata;
-	int id = pdev->id;
+	int in_use, id = pdev->id;
 
 	ctrl = get_fimc_ctrl(id);
 	pdata = to_fimc_plat(ctrl->dev);
 
-	if (atomic_read(&ctrl->in_use))
+	in_use = atomic_read(&ctrl->in_use);
+
+	if (in_use)
 		fimc_clk_en(ctrl, true);
 
 	if (ctrl->out)
@@ -1635,6 +1637,9 @@ int fimc_resume(struct platform_device *pdev)
 	else
 		ctrl->status = FIMC_STREAMOFF;
 
+	if (in_use && 0 != ctrl->id)
+		fimc_clk_en(ctrl, false);
+	
 	return 0;
 }
 #else
