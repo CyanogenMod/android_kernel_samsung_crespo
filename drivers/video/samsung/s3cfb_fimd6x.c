9e98b9dcd04a8d5ce0f77d32eedca133101d191e
JP Abgrall <jpa@google.com>
2012-01-13 21:49:21 -0800

    video: s3c fimd: Adjust pixclock to match hw settings
    * The pixclock used to report refresh rates to userspace
    did not reflect the actual frequency that the hardware was
    programmed with due to rounding.
    Now we recalculate the pixclock based on what is programmed in
    the VIDCON0 register.
    
    * The div bits in the control register where not being cleared
    before the new value was OR-ed in.
    
    
    Change-Id: Ic417b67a3324d94727ed82170d31015967bb1ce1
    Signed-off-by: JP Abgrall <jpa@google.com>

--- drivers/video/samsung/s3cfb_fimd6x.c
+++ drivers/video/samsung/s3cfb_fimd6x.c
@@ -152,6 +152,19 @@ int s3cfb_frame_off(struct s3cfb_global *ctrl)
 	return 0;
 }
 
+void s3cfb_readjust_pixclock(struct s3cfb_global *ctrl, u32 src_clk, u32 div)
+{
+	struct s3c_platform_fb *pdata = to_fb_plat(ctrl->dev);
+	int i;
+	u32 pixclock;
+
+	pixclock = KHZ2PICOS(src_clk / 1000) * div;
+	dev_info(ctrl->dev, "pixclock adjusted from %d to %d\n",
+		 ctrl->fb[0]->var.pixclock, pixclock);
+	for (i = 0; i < pdata->nr_wins; i++)
+		ctrl->fb[i]->var.pixclock = pixclock;
+}
+
 int s3cfb_set_clock(struct s3cfb_global *ctrl)
 {
 	struct s3c_platform_fb *pdata = to_fb_plat(ctrl->dev);
@@ -162,7 +175,8 @@ int s3cfb_set_clock(struct s3cfb_global *ctrl)
 	/* fixed clock source: hclk */
 	cfg = readl(ctrl->regs + S3C_VIDCON0);
 	cfg &= ~(S3C_VIDCON0_CLKSEL_MASK | S3C_VIDCON0_CLKVALUP_MASK |
-		S3C_VIDCON0_VCLKEN_MASK | S3C_VIDCON0_CLKDIR_MASK);
+		 S3C_VIDCON0_VCLKEN_MASK | S3C_VIDCON0_CLKDIR_MASK |
+		 S3C_VIDCON0_CLKVAL_F(-1));
 	cfg |= (S3C_VIDCON0_CLKVALUP_ALWAYS | S3C_VIDCON0_VCLKEN_NORMAL |
 		S3C_VIDCON0_CLKDIR_DIVIDED);
 
@@ -199,6 +213,7 @@ int s3cfb_set_clock(struct s3cfb_global *ctrl)
 	dev_dbg(ctrl->dev, "parent clock: %d, vclk: %d, vclk div: %d\n",
 			src_clk, vclk, div);
 
+	s3cfb_readjust_pixclock(ctrl, src_clk, div);
 	return 0;
 }
 
