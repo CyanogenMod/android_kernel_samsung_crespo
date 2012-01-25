3dd0e099190b859271090b090ffe6eb7104abae5
Alistair Strachan <alistair.strachan@imgtec.com>
2012-01-25 15:47:41 +0000

    gpu: pvr: Update to DDK 1.8@795947
    Change-Id: I62fe8e0f0c5f4046a4852d8dfb92c8b0991df378

--- drivers/gpu/pvr/pvrversion.h
+++ drivers/gpu/pvr/pvrversion.h
@@ -36,7 +36,7 @@
 
 #define PVRVERSION_FAMILY           "sgxddk"
 #define PVRVERSION_BRANCHNAME       "1.8"
-#define PVRVERSION_BUILD             794370
+#define PVRVERSION_BUILD             795947
 #define PVRVERSION_BSCONTROL        "CustomerGoogle_Android_ogles1_ogles2_GPL"
 
 #define PVRVERSION_STRING           "CustomerGoogle_Android_ogles1_ogles2_GPL sgxddk 18 1.8@" PVR_STR2(PVRVERSION_BUILD)
@@ -45,7 +45,7 @@
 #define COPYRIGHT_TXT               "Copyright (c) Imagination Technologies Ltd. All Rights Reserved."
 
 #define PVRVERSION_BUILD_HI          79
-#define PVRVERSION_BUILD_LO          4370
+#define PVRVERSION_BUILD_LO          5947
 #define PVRVERSION_STRING_NUMERIC    PVR_STR2(PVRVERSION_MAJ) "." PVR_STR2(PVRVERSION_MIN) "." PVR_STR2(PVRVERSION_BUILD_HI) "." PVR_STR2(PVRVERSION_BUILD_LO)
 
 #endif /* _PVRVERSION_H_ */
