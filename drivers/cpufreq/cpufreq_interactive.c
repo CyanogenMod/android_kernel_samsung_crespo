47d6bebfa46b485babd023d2142501ab1e74dbfd
Narayanan Gopalakrishnan <nargop@codeaurora.org>
2012-10-04 03:09:29 -0500

    cpufreq: interactive: add boostpulse duration
    from http://review.cyanogenmod.com/#/c/24324/
    by Daniel Bateman <jetison.24@gmail.com>
    
    This makes up for the portions missing that would've been written
    by Narayanan Gopalakrishnan but they were missing (duration, and boost
    time) and also adds the ability to set duration (which was originally
    created by cyanogen)
    
    Change-Id: I8aadc9a70501862c78e153608f39c6e02746083b

--- drivers/cpufreq/cpufreq_interactive.c
+++ drivers/cpufreq/cpufreq_interactive.c
@@ -69,6 +69,14 @@ static struct mutex set_speed_lock;
 /* Hi speed to bump to from lo speed when load burst (default max) */
 static u64 hispeed_freq;
 
+/* When the boostpulse was activated */
+static u64 boostpulse_boosted_time;
+
+/* How long the boostpulse will remain active */
+#define DEFAULT_BOOSTPULSE_DURATION	500000
+#define MAX_BOOSTPULSE_DURATION		5000000
+static int boostpulse_duration;
+
 /* Go to hi speed when CPU load at or above this value. */
 #define DEFAULT_GO_HISPEED_LOAD 85
 static unsigned long go_hispeed_load;
@@ -138,6 +146,7 @@ static void cpufreq_interactive_timer(unsigned long data)
 	unsigned int new_freq;
 	unsigned int index;
 	unsigned long flags;
+	u64 now = ktime_to_us(ktime_get());
 
 	smp_rmb();
 
@@ -196,7 +205,14 @@ static void cpufreq_interactive_timer(unsigned long data)
 	if (load_since_change > cpu_load)
 		cpu_load = load_since_change;
 
-	if (cpu_load >= go_hispeed_load || boost_val) {
+	if (boostpulse_boosted_time &&
+			now > boostpulse_boosted_time + boostpulse_duration) {
+		/* Disable the boostpulse. */
+		boostpulse_boosted_time = 0;
+		boostpulse_duration = 0;
+	}
+
+	if (cpu_load >= go_hispeed_load || boost_val || boostpulse_boosted_time) {
 		if (pcpu->target_freq <= pcpu->policy->min) {
 			new_freq = hispeed_freq;
 		} else {
@@ -787,11 +803,17 @@ static ssize_t store_boostpulse(struct kobject *kobj, struct attribute *attr,
 				const char *buf, size_t count)
 {
 	int ret;
-	unsigned long val;
+	unsigned int val;
 
-	ret = kstrtoul(buf, 0, &val);
+	ret = sscanf(buf, "%u", &val);
 	if (ret < 0)
 		return ret;
+
+	boostpulse_boosted_time = ktime_to_us(ktime_get());
+	if (val > 1 && val <= MAX_BOOSTPULSE_DURATION)
+		boostpulse_duration = val;
+	else
+		boostpulse_duration = DEFAULT_BOOSTPULSE_DURATION;
 
 	trace_cpufreq_interactive_boost("pulse");
 	cpufreq_interactive_boost();
